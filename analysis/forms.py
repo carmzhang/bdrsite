345860d2023076e520940a46dc462709b6413b50
carmzhang <33500462+carmzhang@users.noreply.github.com>
2018-02-14 15:12:55 -0500

    required=FALSE, forms.py

--- analysis/forms.py
+++ analysis/forms.py
@@ -19,8 +19,8 @@ class QueryDropdownForm(forms.Form):
 class FillQueryForm(forms.Form):
 	select = forms.CharField(label="SELECT:", widget=forms.TextInput(attrs={'placeholder': 'Column name(s). Separate using commas.'}))
 	from_field = forms.CharField(label="FROM:", widget=forms.TextInput(attrs={'placeholder': 'Table name(s). If inner join, separate using commas.'}))
-	where = forms.CharField(label="WHERE:", widget=forms.TextInput(attrs={'placeholder': 'Conditions on column values, separated by AND. Leave blank if none.'}))
-	limit = forms.CharField(label="LIMIT:", widget=forms.TextInput(attrs={'placeholder': 'Numerical value limiting number of rows returned.'}))
+	where = forms.CharField(label="WHERE:", required=False, widget=forms.TextInput(attrs={'placeholder': 'Conditions on column values, separated by AND. Leave blank if none.'}))
+	limit = forms.CharField(label="LIMIT:", required=False, widget=forms.TextInput(attrs={'placeholder': 'Numerical value limiting number of rows returned.'}))
 
 class MapForm(forms.Form):
 	print("hello")
